aeab1ef7032872325b519c41fbabf4a875a4ba66
sayali133 <sayalijadhav123@gmail.com>
2026-01-07 18:36:16 +0530

    Add say.py

---
+++ say.py
@@ -0,0 +1,13 @@
+#Execution time tracker
+def decorator(func):
+    def wrapper(*args,**kwargs):
+        print(f"calling {func.__name__}")
+        result=func(*args,**kwargs)
+        print(f"finished add{func.__name__}")
+        return result
+    return wrapper
+
+@decorator
+def add(a,b):
+    return a+b
+add(2,3)
